7351ecac3d7ade70046e310f0741d2acaad4e474
Ashley Lawrence <ash.l.96@outlook.com>
2024-08-05 09:39:34 +0000

    Add favicon, and final tidy ups for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 runGame(gameType);
             }
         })
+
     }
 
-runGame("addition")
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
+    runGame("addition")
 
 })
 
@@ -24,6 +31,8 @@ runGame("addition")
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+    document.getElementById("answer-box").value = '';
+    document.getElementById("answer-box").focus();
 
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
